0dde7a24321e16c263bd72a78b71ef656e126e2d
Marius Ducea <marius.ducea@gmail.com>
2010-09-27 17:50:27 -0700

    node version updated

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "marius@promethost.com"
 license          "Apache 2.0"
 description      "Installs/Configures nodejs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.1.3"
 recipe           "nodejs", "Installs Node.JS from source"
 recipe           "nodejs::npm", "Installs npm - a package manager for node"
 
