00f6e8497f9ae5c7504660137796227ed19117a7
Marius Ducea <marius.ducea@gmail.com>
2011-02-18 14:36:01 -0800

    [nodejs]: bump metadata version for release on the cookbooks site

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "marius@promethost.com"
 license          "Apache 2.0"
 description      "Installs/Configures nodejs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.0"
+version          "0.4.0"
 recipe           "nodejs", "Installs Node.JS from source"
 recipe           "nodejs::npm", "Installs npm - a package manager for node"
 
