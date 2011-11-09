720c089a818726aa38668e64459019d4dd797418
Rohan Singh <rohan@washington.edu>
2011-11-09 11:38:47 -0800

    Update to node 0.6.0 and 1.0.104.

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,6 @@
 # limitations under the License.
 #
 
-default.nodejs[:version] = "0.4.8"
+default.nodejs[:version] = "0.6.0"
 default.nodejs[:dir] = "/usr/local"
-default.nodejs[:npm] = "1.0.10"
+default.nodejs[:npm] = "1.0.104"
