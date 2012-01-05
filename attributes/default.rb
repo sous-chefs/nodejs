e25ee84d6293e290e840e58f2d310a4a0bc0d392
George Miroshnykov <george.miroshnykov@gmail.com>
2012-01-05 13:37:50 +0200

    [nodejs]: bump nodejs and npm versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,6 @@
 # limitations under the License.
 #
 
-default.nodejs[:version] = "0.6.5"
+default.nodejs[:version] = "0.6.7"
 default.nodejs[:dir] = "/usr/local"
-default.nodejs[:npm] = "1.0.105"
+default.nodejs[:npm] = "1.0.106"
