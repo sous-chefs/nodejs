e6c3f6b6961fa35ee9985837dbec35c3cb53dac8
George Miroshnykov <george.miroshnykov@gmail.com>
2012-01-21 01:47:27 +0200

    [nodejs]: made download, compilation and installation a bit more granular.

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,7 @@
 # limitations under the License.
 #
 
-default.nodejs[:version] = "0.6.5"
+default.nodejs[:version] = "0.6.8"
+default.nodejs[:checksum] = "e6cbfc5ccdbe10128dbbd4dc7a88c154d80f8a39c3a8477092cf7d25eef78c9c"
 default.nodejs[:dir] = "/usr/local"
 default.nodejs[:npm] = "1.0.105"
