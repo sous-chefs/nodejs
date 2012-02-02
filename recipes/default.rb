0af3348ef2ce9d77d8e04b8dc07222985d543a72
Marius Ducea <marius.ducea@gmail.com>
2012-02-02 08:13:32 -0800

    fix not_if check in the make install stage to actually work

--- recipes/default.rb
+++ recipes/default.rb
@@ -58,5 +58,5 @@ end
 
 execute "make install" do
   cwd "/usr/local/src/node-v#{node[:nodejs][:version]}"
-  not_if "test `#{node[:nodejs][:dir]}/bin/node` == 'v#{node[:nodejs][:version]}'"
+  not_if { `#{node[:nodejs][:dir]}/bin/node --version`.chomp == "v#{node[:nodejs][:version]}" }
 end
