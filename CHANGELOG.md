388c71bcfbd1cd3a0fdb70b6d0d1b5022f5a73c6
Marius Ducea <marius.ducea@gmail.com>
2012-08-14 01:15:49 -0700

    bump version to 1.0.1 for release on community site

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## v1.0.1:
+
+ * fixed bug that prevented overwritting the node/npm versions (moved the `src_url`s as local variables instead of attributes) - thanks @johannesbecker
+ * updated the default versions to the latest node/npm
+
 ## v1.0.0:
 
 * added packages installation support thanks to Nathan L Smith
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "marius@promethost.com"
 license          "Apache 2.0"
 description      "Installs/Configures nodejs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 recipe "nodejs", "Installs Node.JS based on the default installation method"
 recipe "nodejs::install_from_source", "Installs Node.JS from source"
