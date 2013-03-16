4bbab7947eb6f193a8664050ddbb87b749fe4ad2
Marius Ducea <marius.ducea@gmail.com>
2013-03-16 01:09:43 -0700

    0.10.0 is the default new stable version

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,8 +18,8 @@
 #
 
 default['nodejs']['install_method'] = 'source'
-default['nodejs']['version'] = '0.8.21'
-default['nodejs']['checksum'] = 'e526f56d22bb2ebee5a607bd1e7a16dcc8530b916e3a372192e6cd5fa97d08e6'
+default['nodejs']['version'] = '0.10.0'
+default['nodejs']['checksum'] = '1624dc37866ebfb5431e3393e6b049cf238cac8ad4d20c6d567263b1259177ab'
 default['nodejs']['checksum_linux_x64'] = 'eaedcf7e3e443cf2fa35f834ed62b334885dc20fcbc7a32ea34e8e85f81b2533'
 default['nodejs']['checksum_linux_x86'] = 'ea4508e4df3c74d964a02d5740374b54f8192af19db518163c77ee7ff318daa7'
 default['nodejs']['dir'] = '/usr/local'
