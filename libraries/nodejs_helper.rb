816ff64aa9524f7fb4652c2ea7591544470be6a7
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2014-03-31 13:08:52 +0200

    fixing rubocop offenses

--- libraries/nodejs_helper.rb
+++ libraries/nodejs_helper.rb
@@ -1,15 +1,16 @@
 module NodeJs
   module Helper
     def npm_dist
-      return {'url' => node['nodejs']['npm']['url'], 'version' => nil} if node['nodejs']['npm']['url']
-
-      require "open-uri"
-      require "json"
-
-      result = JSON.parse(URI.parse("https://registry.npmjs.org/npm/#{node['nodejs']['npm']['version']}").read)
-      ret = {'url' => result['dist']['tarball'], 'npm_version' => result['_npmVersion']}
-      Chef::Log.debug("Npm dist #{ret}")
-      return ret
+      if node['nodejs']['npm']['url']
+        return { 'url' => node['nodejs']['npm']['url'], 'version' => nil }
+      else
+        require 'open-uri'
+        require 'json'
+        result = JSON.parse(URI.parse("https://registry.npmjs.org/npm/#{node['nodejs']['npm']['version']}").read)
+        ret = { 'url' => result['dist']['tarball'], 'npm_version' => result['_npmVersion'] }
+        Chef::Log.debug("Npm dist #{ret}")
+        return ret
+      end
     end
 
     def install_not_needed?
