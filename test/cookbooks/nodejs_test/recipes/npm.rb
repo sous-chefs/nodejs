ce43bb2471ebb586f39ee25477dfbf39bd8de504
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-04-10 15:07:12 +0200

    Add integration testing

---
+++ test/cookbooks/nodejs_test/recipes/npm.rb
@@ -0,0 +1,27 @@
+include_recipe 'git'
+
+user 'random' do
+  supports :manage_home => true
+  home '/home/random'
+end
+
+# global "express"
+nodejs_npm 'express'
+
+nodejs_npm 'async' do
+  version '0.6.2'
+end
+
+nodejs_npm 'request' do
+  url 'https://github.com/mikeal/request'
+end
+
+git '/home/random/grunt' do
+  repository 'https://github.com/gruntjs/grunt'
+  user 'random'
+end
+
+nodejs_npm 'grunt' do
+  path '/home/random/grunt'
+  user 'random'
+end
