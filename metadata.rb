d7bdd2bd4a8ca01e73dc759b9a5ef97c470fc099
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-04-14 12:33:32 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,12 @@
+name 'nodejs'
 maintainer 'Promet Solutions'
 maintainer_email 'marius@promethost.com'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.0'
-name 'nodejs'
-provides 'nodejs'
 
-recipe 'nodejs', 'Installs Node.JS based on the default installation method'
-recipe 'nodejs::install_from_source', 'Installs Node.JS from source'
-recipe 'nodejs::install_from_binary', 'Installs Node.JS from official binaries'
-recipe 'nodejs::install_from_package', 'Installs Node.JS from packages'
-recipe 'nodejs::npm', 'Installs npm from source - a package manager for node'
+conflicts 'node'
 
 depends 'yum-epel'
 depends 'build-essential'
@@ -21,3 +16,5 @@ depends 'apt'
 %w(debian ubuntu centos redhat smartos).each do |os|
   supports os
 end
+
+suggests 'application_nodejs'
