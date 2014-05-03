ddff6fce64fdd7cd96e44cf5a5f40b455d37b526
Marius Ducea <marius.ducea@gmail.com>
2014-05-02 17:04:22 -0700

    fix correct building of the download url; without this it was dropping dist and returned 404

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,7 +26,7 @@ end
 
 default['nodejs']['version'] = '0.10.26'
 
-default['nodejs']['prefix_url'] = ::URI.join('http://nodejs.org/dist', "v#{node['nodejs']['version']}").to_s
+default['nodejs']['prefix_url'] = ::URI.join('http://nodejs.org/dist/', "v#{node['nodejs']['version']}/").to_s
 
 default['nodejs']['install_repo'] = true
 
@@ -37,3 +37,5 @@ default['nodejs']['binary']['url'] = nil # Auto generated
 default['nodejs']['binary']['checksum']['linux_x64'] = '305bf2983c65edea6dd2c9f3669b956251af03523d31cf0a0471504fd5920aac'
 default['nodejs']['binary']['checksum']['linux_x86'] = '8fa2d952556c8b5aa37c077e2735c972c522510facaa4df76d4244be88f4dc0f'
 default['nodejs']['binary']['checksum']['linux_arm-pi'] = '561ec2ebfe963be8d6129f82a7d1bc112fb8fbfc0a1323ebe38ef55850f25517'
+
+default['nodejs']['make_threads'] = node['cpu'] ? node['cpu']['total'].to_i : 2
