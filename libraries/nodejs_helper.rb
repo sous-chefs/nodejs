938d0ff67b9636c80ae81f22196f28ea254fd468
Richard Li <yli@library.tamu.edu>
2014-05-21 13:01:27 -0500

    When installs non-globally from npm registry, list is nil initially when no package was installed locally

--- libraries/nodejs_helper.rb
+++ libraries/nodejs_helper.rb
@@ -33,7 +33,7 @@ module NodeJs
     def npm_package_installed?(package, version = nil, path = nil)
       list = npm_list(path)['dependencies']
       # Return true if package installed and installed to good version
-      list.key?(package) && (version ? list[package]['version'] == version : true)
+      (!list.nil?) && list.key?(package) && (version ? list[package]['version'] == version : true)
     end
   end
 end
