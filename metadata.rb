f47a6ceed96d5f8d742bef235ed1f711548dfc5e
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-07-30 14:27:59 +0200

    Bump devodd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'marius@promethost.com'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.0'
+version '2.0.1'
 
 conflicts 'node'
 
