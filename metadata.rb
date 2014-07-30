4069e3251dc45078971f58027185b21f684069e3
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-07-30 13:08:17 +0200

    Release 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'marius@promethost.com'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.3.0'
+version '2.0.0'
 
 conflicts 'node'
 
