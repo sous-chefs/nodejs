b2d1e4401833a6665300847b8166986fa8294267
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-08-21 15:13:36 +0200

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'nodejs'
-maintainer 'Promet Solutions'
-maintainer_email 'marius@promethost.com'
+maintainer 'redguide'
+maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
