548b7d286a9f26f03318df2878a925b349430890
Guilhem Lettron <guilhem.lettron@gmail.com>
2014-10-27 23:21:16 +0100

    Release 2.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures nodejs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.1'
+version '2.2.0'
 
 conflicts 'node'
 
