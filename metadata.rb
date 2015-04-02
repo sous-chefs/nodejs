4dd9f5191299cbf1f4570589bc7956b871d6b319
Guilhem Lettron <guilhem@lettron.fr>
2015-04-02 16:17:53 +0200

    Release 2.4.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures node.js & io.js'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.3.3'
+version '2.4.0'
 
 conflicts 'node'
 
