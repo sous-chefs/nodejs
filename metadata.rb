abdba3b8487cafd8ed99d1d4d82fd98c301e2cdb
Guilhem Lettron <guilhem@lettron.fr>
2015-04-02 16:18:37 +0200

    Bump devodd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures node.js & io.js'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.4.0'
+version '2.4.1'
 
 conflicts 'node'
 
