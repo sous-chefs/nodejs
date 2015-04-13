aa9ae8e015cef5209776462395fbb13f13c39550
Emil Diaz <emil@chicoryapp.com>
2015-04-12 23:14:39 -0400

    Fix to handle Chef 12 metadata attributes in Chef 11

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache 2.0'
 description 'Installs/Configures node.js & io.js'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-source_url 'https://github.com/redguide/nodejs'
-issues_url 'https://github.com/redguide/nodejs/issues'
-version '2.4.1'
+source_url 'https://github.com/redguide/nodejs'		if respond_to?(:source_url)
+issues_url 'https://github.com/redguide/nodejs/issues'	if respond_to?(:issues_url)
+version '2.4.2'
 
 conflicts 'node'
 
