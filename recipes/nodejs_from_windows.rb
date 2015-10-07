2ffb70b3a2a3d16223caa3bb1b1d740cbbfeb130
Ashok Manji <amanji@expedia.com>
2015-10-07 16:31:17 +0100

    Adding Windows Support

---
+++ recipes/nodejs_from_windows.rb
@@ -0,0 +1,18 @@
+#
+# Author:: Ashok Manji (ashokmanji@hotmail.com)
+# Cookbook Name:: nodejs
+# Recipe:: windows
+#
+
+arch           = node['kernel']['machine'] =~ /x86_64/ ? "x64" : "x86"
+version        = "v#{node['nodejs']['version']}/"
+prefix         = node['nodejs']['prefix_url'][node['nodejs']['engine']]
+filename       = "node-v#{node['nodejs']['version']}-windows-#{arch}.msi"
+nodejs_bin_url = ::URI.join(prefix, version, filename).to_s
+checksum       = node['nodejs']['binary']['checksum']["windows_#{arch}"]
+
+windows_package 'Nodejs' do
+  source nodejs_bin_url
+  checksum checksum
+  action :install
+end
