b34c9b2fa978a763d783292fcf19a95007c199b8
Gregory McCue <gsmccue@gmail.com>
2016-12-02 12:01:56 +0100

    Install npm from repo.

--- attributes/packages.rb
+++ attributes/packages.rb
@@ -4,7 +4,7 @@ include_attribute 'nodejs::repo'
 case node['nodejs']['engine']
 when 'node'
   default['nodejs']['packages'] = value_for_platform_family(
-    'debian' => node['nodejs']['install_repo'] ? ['nodejs'] : ['nodejs', 'npm', 'nodejs-dev'],
+    'debian' => ['nodejs', 'npm', 'nodejs-dev'],
     %w(rhel fedora) => ['nodejs', 'nodejs-devel', 'npm'],
     'mac_os_x' => ['node'],
     'freebsd' => %w(node npm),
