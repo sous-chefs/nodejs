9d18360c6b76da9edc7b499b96785a05d4a42ba2
Tim Smith <tsmith@chef.io>
2017-04-27 21:31:53 -0700

    Require a more recent compat_resource and fix license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'nodejs'
 maintainer 'redguide'
 maintainer_email 'guilhem@lettron.fr'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures node.js & io.js'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/redguide/nodejs'
@@ -12,7 +12,7 @@ version '3.0.1'
 depends 'yum-epel'
 depends 'build-essential'
 depends 'ark', '>= 2.0.2'
-depends 'compat_resource', '>= 12.14.7'
+depends 'compat_resource', '>= 12.16'
 
 %w(debian ubuntu centos redhat scientific oracle amazon smartos mac_os_x opensuse opensuseleap suse).each do |os|
   supports os
