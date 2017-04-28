a6501e3eed353f6832b4f37917f8461e74177ea8
Tim Smith <tsmith@chef.io>
2017-04-27 22:17:33 -0700

    Set the default repos to Node 6 and iojs 3
    The repo URLs
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -4,7 +4,7 @@ when 'node'
   when 'debian'
     default['nodejs']['install_repo'] = true
 
-    default['nodejs']['repo']      = 'https://deb.nodesource.com/node'
+    default['nodejs']['repo']      = 'https://deb.nodesource.com/node_6.x'
     default['nodejs']['keyserver'] = 'keyserver.ubuntu.com'
     default['nodejs']['key']       = '1655a0ab68576280'
   when 'rhel', 'amazon'
@@ -15,7 +15,7 @@ when 'iojs'
   when 'debian'
     default['nodejs']['install_repo'] = true
 
-    default['nodejs']['repo']      = 'https://deb.nodesource.com/iojs_2.x'
+    default['nodejs']['repo']      = 'https://deb.nodesource.com/iojs_3.x'
     default['nodejs']['keyserver'] = 'keyserver.ubuntu.com'
     default['nodejs']['key']       = '1655a0ab68576280'
   end
