e4cdbf4246cf62fed8123b71715090b3f86d4255
Tim Smith <tsmith@chef.io>
2017-11-15 10:34:13 -0800

    Release 5.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/redguide/nodejs'
 issues_url 'https://github.com/redguide/nodejs/issues'
 chef_version '>= 12.14' if respond_to?(:chef_version)
-version '4.0.0'
+version '5.0.0'
 
 depends 'build-essential'
 depends 'ark', '>= 2.0.2'
