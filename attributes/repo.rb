2aa796f08e64ba04a593662b7d428ac76d06ade4
Brian Dwyer <2973273+bdwyertech@users.noreply.github.com>
2018-06-05 16:26:00 -0400

    Fix for hardcoded Repo URL's

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,12 +1,16 @@
+# Grab Node.JS Major Version
+node_major = node['nodejs']['version'].to_i
+
 case node['platform_family']
 when 'debian'
   default['nodejs']['install_repo'] = true
-  default['nodejs']['repo']         = 'https://deb.nodesource.com/node_6.x'
+  default['nodejs']['repo']         = "https://deb.nodesource.com/node_#{node_major}.x"
   default['nodejs']['keyserver']    = 'keyserver.ubuntu.com'
   default['nodejs']['key']          = '1655a0ab68576280'
 when 'rhel', 'amazon'
   default['nodejs']['install_repo'] = true
   release_ver = platform?('amazon') ? 6 : node['platform_version'].to_i
-  default['nodejs']['repo']         = "https://rpm.nodesource.com/pub_6.x/el/#{release_ver}/$basearch"
+
+  default['nodejs']['repo']         = "https://rpm.nodesource.com/pub_#{node_major}.x/el/#{release_ver}/$basearch"
   default['nodejs']['key']          = 'https://rpm.nodesource.com/pub/el/NODESOURCE-GPG-SIGNING-KEY-EL'
 end
