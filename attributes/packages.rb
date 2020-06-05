e5cebf3a766863776773d8bb91a3d299c2b12aee
Tim Smith <tsmith84@gmail.com>
2020-06-04 19:56:35 -0700

    Update array format
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/packages.rb
+++ attributes/packages.rb
@@ -2,9 +2,9 @@ include_attribute 'nodejs::default'
 include_attribute 'nodejs::repo'
 
 default['nodejs']['packages'] = value_for_platform_family(
-  'debian' => node['nodejs']['install_repo'] ? ['nodejs'] : ['nodejs', 'npm', 'nodejs-dev'],
-  %w(rhel fedora amazon) => node['nodejs']['install_repo'] ? ['nodejs', 'nodejs-devel'] : ['nodejs', 'npm', 'nodejs-dev'],
-  'suse' => node['platform_version'].to_i < 42 ? ['nodejs', 'nodejs-devel'] : ['nodejs4', 'npm4', 'nodejs4-devel'],
+  'debian' => node['nodejs']['install_repo'] ? ['nodejs'] : %w(nodejs npm nodejs-dev),
+  %w(rhel fedora amazon) => node['nodejs']['install_repo'] ? %w(nodejs nodejs-devel) : %w(nodejs npm nodejs-dev),
+  'suse' => node['platform_version'].to_i < 42 ? %w(nodejs nodejs-devel) : %w(nodejs4 npm4 nodejs4-devel),
   'mac_os_x' => ['node'],
   'freebsd' => %w(node npm),
   'default' => ['nodejs']
