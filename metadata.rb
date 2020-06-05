2785cf4e10d6e129356b77f22072ca23752446d9
Tim Smith <tsmith84@gmail.com>
2020-06-04 20:09:06 -0700

    Disable build-essential cop
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ issues_url 'https://github.com/redguide/nodejs/issues'
 chef_version '>= 12.14'
 version '6.0.0'
 
-depends 'build-essential', '>= 5.0'
+depends 'build-essential', '>= 5.0' # cookstyle: disable ChefModernize/UnnecessaryDependsChef14
 depends 'ark', '>= 2.0.2'
 
 %w(debian ubuntu centos redhat scientific oracle amazon smartos mac_os_x opensuseleap suse).each do |os|
