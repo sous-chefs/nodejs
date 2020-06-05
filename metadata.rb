aa813af8bf9d10c1ab7d340785e92ff5c975dbb1
Tim Smith <tsmith84@gmail.com>
2020-06-04 19:55:06 -0700

    Remove long_description in metadata
    Not needed
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer 'redguide'
 maintainer_email 'guilhem@lettron.fr'
 license 'Apache-2.0'
 description 'Installs/Configures node.js'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/redguide/nodejs'
 issues_url 'https://github.com/redguide/nodejs/issues'
 chef_version '>= 12.14' if respond_to?(:chef_version)
