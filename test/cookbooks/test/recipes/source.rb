8dfb2c38f0ad55af3bc4ee40cbd0b6862773e1d9
Dmitry Zhmurko <zhmurko@gmail.com>
2020-10-21 10:06:05 +0300

    install devtoolset-7-gcc-c++

--- test/cookbooks/test/recipes/source.rb
+++ test/cookbooks/test/recipes/source.rb
@@ -3,18 +3,10 @@ apt_update 'update'
 # Source install puts the npm symlink in /usr/local/bin
 ENV['PATH'] = '/usr/local/bin:' + ENV['PATH']
 
-if platform_family?('rhel') && node['platform_version'].to_i >= 7
+if platform_family?('rhel') && node['platform_version'].to_i == 7
   include_recipe 'test::source_centos7'
 end
 
 node.default['nodejs']['install_method'] = 'source'
-# include_recipe 'nodejs::default'
-# include_recipe 'test::resource'
-
-execute 'check results' do
-  command 'rpm -ql devtoolset-7-gcc'\
-    ' && ls -la /opt/rh/devtoolset-7/root/usr/bin/'\
-    ' && which g++'\
-    ' && g++ -v'\
-    ' && exit 1'
-end
+include_recipe 'nodejs::default'
+include_recipe 'test::resource'
--- test/cookbooks/test/recipes/source_centos7.rb
+++ test/cookbooks/test/recipes/source_centos7.rb
@@ -4,8 +4,8 @@ node.default['yum']['centos-sclo-rh']['enabled'] = true
 node.default['yum']['centos-sclo-rh']['managed'] = true
 include_recipe 'yum-centos'
 
-yum_package 'devtoolset-7-gcc*'
+yum_package 'devtoolset-7-gcc-c++'
 
-ENV['PATH'] = '/opt/rh/devtools-7/root/usr/bin:' + ENV['PATH']
+ENV['PATH'] = '/opt/rh/devtoolset-7/root/usr/bin:' + ENV['PATH']
 ENV['CC'] = '/opt/rh/devtoolset-7/root/usr/bin/gcc'
 ENV['CXX'] = '/opt/rh/devtoolset-7/root/usr/bin/g++'
